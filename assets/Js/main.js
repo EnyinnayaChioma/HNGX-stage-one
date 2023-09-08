37bc13122ffab056f8572ff7cac4a3f79ac230bc
Chioma <enyinnayachioma738@gmail.com>
2023-09-08 20:48:23 +0100

    i add my alt attribute

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,5 +1,15 @@
 // Js for the current UTC time
-document.getElementById("time").textContent =`This is the time in milliseconds ${new Date().getUTCMilliseconds()}`
+// document.getElementById("time").textContent =`This is the time in milliseconds ${new Date().getUTCMilliseconds()}`
+
+const currentTime = new Date();
+const utcTime = `${currentTime.getUTCHours()}:${currentTime.getUTCMinutes()}:${currentTime.getUTCSeconds()}`;
+document.getElementById("time").textContent = `The time is ${utcTime}`;
+
+
+
+
+
+
 
 // Js for the day of the week
 let daysOfTheWeek = ['Sunday','Monday', 'Tuesday', 'Wednessday','Thursday','Friday','Saturday'];
